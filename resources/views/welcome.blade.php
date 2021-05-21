f4c255289c56dd97c368f08e66015b95575ab079
Wilson Agrela <wilson-andre100@hotmail.com>
2021-05-21 16:55:45 +0100

    Fisrt commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body text-center">
+                    <h3>Welcome to NearTicket</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

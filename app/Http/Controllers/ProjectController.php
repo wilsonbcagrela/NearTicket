92f33bf505a882bc13dd1c88522dc1bb1ab58d97
Wilson Agrela <wilson-andre100@hotmail.com>
2021-07-07 11:48:17 +0100

    user and admins can see their projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -11,9 +11,24 @@ class ProjectController extends Controller
         $sessionID = session('id');
         $sessionClient_id = session('Client_id');
         $responseUser = Http::get("http://localhost:8080/user/projects?Client_id={$sessionClient_id}&id={$sessionID}");
-        $projects = json_decode($responseUser->body());
-        return view('home', [
-            "projects" => $projects
-        ]);
+        $responseAdmin = Http::get("http://localhost:8080/helpdesk/projects?id={$sessionID}");
+        $responseClient= Http::get("http://localhost:8080/client/projects?Client_id={$sessionClient_id}");
+        if(session('TypeUser') == "User"){
+            $projects = json_decode($responseUser->body());
+            return view('home', [
+                "projects" => $projects
+            ]);
+        }else if(session('TypeUser') == "Admin"){
+            $projects = json_decode($responseAdmin->body());
+            return view('home', [
+                "projects" => $projects
+            ]);
+        }else{//fixed this
+            $projects = json_decode($responseClient->body());
+            return view('home', [
+                "projects" => $projects
+            ]);
+        }
+
     }
 }
